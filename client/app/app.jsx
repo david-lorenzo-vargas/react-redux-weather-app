8260dd33c2db892349156e82df3daac77eccfa8b
unknown <dlorenzo.05@gmail.com>
2021-02-03 00:36:07 +0100

    App file created

---
+++ client/app/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './reset.scss';
+import { Page } from '@components';
+
+const App = () => (
+  <div className={styles}>
+    <Page />
+  </div>
+);
+
+export default App;
