e4b68a135e7c3b01dd39e9f5faa66b78c9fb4d39
unknown <dlorenzo.05@gmail.com>
2021-02-03 16:02:58 +0100

    Provider imported from react-reduxt to connect the redux store

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import styles from './reset.scss';
 import WeatherAppContainer from './container';
 
+import store from './state';
+
 const App = () => (
   <div className={styles}>
-    <WeatherAppContainer />
+    <Provider store={store}>
+      <WeatherAppContainer />
+    </Provider>
   </div>
 );
 
