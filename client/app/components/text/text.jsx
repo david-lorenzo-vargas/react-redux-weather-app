010b65be32f5f7844c17c7f0bffd746cc1679223
unknown <dlorenzo.05@gmail.com>
2021-02-03 10:57:25 +0100

    text.jsx file created for Text component

---
+++ client/app/components/text/text.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
+
+import styles from './text.scss';
+
+const cx = classnames.bind(styles);
+
+const Text = (props) => {
+  const {
+    text,
+    size,
+    color,
+    uppercase,
+  } = props;
+
+  return (
+    <span className={cx('text', {
+      [`text--size-${size}`]: size,
+      [`text--color-${color}`]: color,
+      'text--uppercase': uppercase,
+    })}
+    >
+      {text}
+    </span>
+  );
+};
+
+Text.propTypes = {
+  text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  size: PropTypes.string,
+  color: PropTypes.string,
+  uppercase: PropTypes.bool,
+};
+
+export default Text;
