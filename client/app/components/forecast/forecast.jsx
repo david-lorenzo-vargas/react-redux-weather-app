6705a097871a1855f61d64002b766225b26bc35e
unknown <dlorenzo.05@gmail.com>
2021-02-04 21:31:25 +0100

    function added to covert a 40 items array received from API to a 5 day forecast array

--- client/app/components/forecast/forecast.jsx
+++ client/app/components/forecast/forecast.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
+import { getDay } from '../../utilities/get-day';
 import styles from './forecast.scss';
 
 import { ForecastItem } from '@components';
 
+const weekReduce = (weekForecast) => {
+  return weekForecast.reduce((acc, currentValue) => {
+    if (acc.length !== 0 ) {
+      if (getDay(acc[acc.length -1].dt) === getDay(currentValue.dt)) {
+        return acc;
+      }
+    }
+    acc = [...acc, currentValue];
+    return acc;
+  }, []);
+};
+
 const Forecast = (props) => {
   const { weekForecast, icon } = props;
 
-  const hasData = weekForecast && !!weekForecast.length
+  const hasData = weekForecast && !!weekForecast.length;
+
+  const weekDays = hasData ? weekReduce(weekForecast) : [];
 
   return (
     <div className={styles['forecast']}>
-      {hasData && (
-        weekForecast.map((item, index) => (
-          <ForecastItem
-            key={index}
-            timestamp={item.dt}
-            temperature={item.main.temp}
-            icon={icon}
-          />
-        ))
-      )}
+      {weekDays.map((item, index) => (
+        <ForecastItem
+          key={index}
+          timestamp={item.dt}
+          temperature={item.main.temp}
+          icon={item.weather[0].icon}
+        />
+      ))}
     </div>
   );
 };
