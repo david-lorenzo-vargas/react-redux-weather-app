89c4b08f2622b45f41dbb3c740e1025bbb701919
unknown <dlorenzo.05@gmail.com>
2021-02-04 12:11:55 +0100

    unused props removed, react-live-clock library imported and clock rendered

--- client/app/components/time/time.jsx
+++ client/app/components/time/time.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import Clock from 'react-live-clock';
 import PropTypes from 'prop-types';
 import styles from './time.scss';
 
 import { Row, Column, Text, Dots } from '@components';
 
-const Time = (props) => {
-  const { time } = props;
-
-  return (
+const Time = () => (
     <div className={styles['time-box']}>
       <Row between>
         <Column>
           <Dots number={['three', 'two', 'one']} />
         </Column>
         <Column>
-          <Text text={time} color="white" size="medium" />
+          <Clock
+            format={'HH:mm:ss'}
+            ticking={true}
+            timezone={'Etc/GMT0'}
+            className={styles['time-box__clock']}
+          />
         </Column>
         <Column>
           <Text text="gmt" uppercase color="white" size="medium" />
@@ -25,7 +28,6 @@ const Time = (props) => {
       </Row>
     </div>
   );
-};
 
 Time.propTypes = {
   time: PropTypes.number,
