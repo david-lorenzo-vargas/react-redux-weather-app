d44229c3353d60b597f2d8ffef82a932a1ac1ad5
unknown <dlorenzo.05@gmail.com>
2021-02-07 00:02:30 +0100

    indentation fixed

--- client/app/components/icon/icon.jsx
+++ client/app/components/icon/icon.jsx
@@ -6,16 +6,21 @@ import styles from './icon.scss';
 
 const cx = classnames.bind(styles);
 
-const Icon = ({icon, countdown}) => (
+const Icon = ({ icon, countdown }) => (
   <div className={cx('icon', {
     'icon--animated': countdown === 60,
-  })}>
-    <img src={`http://openweathermap.org/img/wn/${icon}.png`} />
+  })}
+  >
+    <img
+      src={`http://openweathermap.org/img/wn/${icon}.png`}
+      alt="weather icon"
+    />
   </div>
 );
 
 Icon.propTypes = {
-  name: PropTypes.string,
+  icon: PropTypes.string,
+  countdown: PropTypes.number,
 };
 
 export default Icon;
