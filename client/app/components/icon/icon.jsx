4afccae200b1ba50361b3c446cd93782d00331ea
unknown <dlorenzo.05@gmail.com>
2021-02-06 21:55:46 +0100

    classnames imported to make the animated class dynamic, we add the animation only when countdown equals to 60 wich means that is the frst render or one minute has passed

--- client/app/components/icon/icon.jsx
+++ client/app/components/icon/icon.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import classnames from 'classnames/bind';
 import PropTypes from 'prop-types';
 
 import styles from './icon.scss';
 
+const cx = classnames.bind(styles);
+
 const Icon = (props) => {
-  const {icon} = props;
+  const {icon, countdown } = props;
 
   return (
-    <div className={styles['icon']}>
+    <div className={cx('icon', {
+      'icon--animated': countdown === 60,
+    })}>
       <img src={`http://openweathermap.org/img/wn/${icon}.png`} />
     </div>
   )
