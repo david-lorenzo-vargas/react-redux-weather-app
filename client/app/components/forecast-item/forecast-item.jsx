c684dd10876bae5819addef8f8e02b8e7d3b5b89
unknown <dlorenzo.05@gmail.com>
2021-02-06 23:29:14 +0100

    code refactoring

--- client/app/components/forecast-item/forecast-item.jsx
+++ client/app/components/forecast-item/forecast-item.jsx
@@ -10,52 +10,53 @@ import styles from './forecast-item.scss';
 
 const cx = classnames.bind(styles);
 
-const ForecastItem = (props) => {
-  const { timestamp, temperature, icon, countdown } = props;
-
-  return (
-    <div className={styles['weather-container']}>
-      <div className={cx('weather-container__item', {
-        'weather-container__item--animated': countdown === 60,
-      })}>
-        <Row between>
-          <Column grow>
-            <div className={styles['weather']}>
-              <Row between>
-                <Column>
-                  <Text
-                    text={getDay(timestamp)}
-                    color="white"
-                    size="large"
-                    uppercase
-                    margin
-                    type="day"
-                    countdown={countdown}
-                  />
-                </Column>
-                <Column>
-                  <Text
-                    text={`${roundNumbers(temperature)}°C`}
-                    color="white"
-                    size="large"
-                    margin
-                    type="temperature"
-                    countdown={countdown}
-                  />
-                </Column>
-              </Row>
-            </div>
-          </Column>
-          <Column shrink>
-            <div className={styles['icon']}>
-              <Icon icon={icon} countdown={countdown} />
-            </div>
-          </Column>
-        </Row>
-      </div>
+const ForecastItem = ({
+  timestamp,
+  temperature,
+  icon,
+  countdown,
+}) => (
+  <div className={styles['weather-container']}>
+    <div className={cx('weather-container__item', {
+      'weather-container__item--animated': countdown === 60,
+    })}>
+      <Row between>
+        <Column grow>
+          <div className={styles['weather']}>
+            <Row between>
+              <Column>
+                <Text
+                  text={getDay(timestamp)}
+                  color="white"
+                  size="large"
+                  uppercase
+                  margin
+                  type="day"
+                  countdown={countdown}
+                />
+              </Column>
+              <Column>
+                <Text
+                  text={`${roundNumbers(temperature)}°C`}
+                  color="white"
+                  size="large"
+                  margin
+                  type="temperature"
+                  countdown={countdown}
+                />
+              </Column>
+            </Row>
+          </div>
+        </Column>
+        <Column shrink>
+          <div className={styles['icon']}>
+            <Icon icon={icon} countdown={countdown} />
+          </div>
+        </Column>
+      </Row>
     </div>
-  );
-};
+  </div>
+);
 
 ForecastItem.propTypes = {
   timestamp: PropTypes.number,
