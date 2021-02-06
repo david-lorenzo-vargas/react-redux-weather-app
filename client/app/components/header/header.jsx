04bc1f08efaa753935ffc46a5d90e96c015fce38
unknown <dlorenzo.05@gmail.com>
2021-02-06 23:29:04 +0100

    code refactoring

--- client/app/components/header/header.jsx
+++ client/app/components/header/header.jsx
@@ -5,31 +5,27 @@ import styles from './header.scss';
 import { roundNumbers } from '../../utilities/round-numbers';
 import { Row, Column, Time, Text, ProgressBar } from '@components';
 
-const Header = (props) => {
-  const { temperature, city, countdown } = props;
-
-  return (
-    <div className={styles['header']}>
-      <Column>
-        <Row between>
-          <Column>
-            <Text text={city} size="large" color="white" />
-          </Column>
-          <Column>
-            <Time />
-          </Column>
-          <Column>
-            <Text text={`${roundNumbers(temperature.temp)} °C`} size="large" color="white" type="temperature" />
-          </Column>
-        </Row>
-      </Column>
-      <Column>
-        <Text text={`reloading in ${countdown}s`} size="small" color="white" />
-        <ProgressBar countdown={countdown} />
-      </Column>
-    </div>
-  );
-};
+const Header = ({temperature, city, countdown }) => (
+  <div className={styles['header']}>
+    <Column>
+      <Row between>
+        <Column>
+          <Text text={city} size="large" color="white" />
+        </Column>
+        <Column>
+          <Time />
+        </Column>
+        <Column>
+          <Text text={`${roundNumbers(temperature.temp)} °C`} size="large" color="white" type="temperature" />
+        </Column>
+      </Row>
+    </Column>
+    <Column>
+      <Text text={`reloading in ${countdown}s`} size="small" color="white" />
+      <ProgressBar countdown={countdown} />
+    </Column>
+  </div>
+);
 
 Header.propTypes = {
   temperature: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
