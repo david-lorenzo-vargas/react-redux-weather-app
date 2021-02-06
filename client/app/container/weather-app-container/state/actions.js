5e336091e18faad47edc414b77e87af9751a7f4b
unknown <dlorenzo.05@gmail.com>
2021-02-06 20:20:10 +0100

    naming changed

--- client/app/container/weather-app-container/state/actions.js
+++ client/app/container/weather-app-container/state/actions.js
@@ -1,7 +1,7 @@
 import * as CONSTANTS from './constants';
 import {
   fetchFiveDayForecastData,
-  fetchCurrentForecastData,
+  fetchCurrentWeatherData,
 } from './api';
 
 export const fetchFiveDayForecastDataSuccess = (data) => ({
@@ -9,8 +9,8 @@ export const fetchFiveDayForecastDataSuccess = (data) => ({
   payload: data,
 });
 
-export const fetchCurrentForecastDataSuccess = (data) => ({
-  type: CONSTANTS.FETCH_CURRENT_FORECAST_DATA_SUCCESS,
+export const fetchCurrentWeatherDataSuccess = (data) => ({
+  type: CONSTANTS.FETCH_CURRENT_WEATHER_DATA_SUCCESS,
   payload: data,
 });
 
@@ -18,16 +18,16 @@ export const fetchFiveDayForecastDataPending = () => ({
   type: CONSTANTS.FETCH_FIVE_DAY_DATA_PENDING,
 });
 
-export const fetchCurrentForecastPending = () => ({
-  type: CONSTANTS.FETCH_CURRENT_FORECAST_DATA_PENDING,
+export const fetchCurrentWeatherPending = () => ({
+  type: CONSTANTS.FETCH_CURRENT_WEATHER_DATA_PENDING,
 });
 
 export const fetchFiveDayForecastDataReject = () => ({
   type: CONSTANTS.FETCH_FIVE_DAY_DATA_REJECT,
 });
 
-export const fetchCurrentForecastDataReject = () => ({
-  type: CONSTANTS.FETCH_CURRENT_FORECAST_DATA_REJECT,
+export const fetchCurrentWeatherDataReject = () => ({
+  type: CONSTANTS.FETCH_CURRENT_WEATHER_DATA_REJECT,
 });
 
 export const fetchFiveDayForecast = (payload) => (dispatch) => {
@@ -38,10 +38,10 @@ export const fetchFiveDayForecast = (payload) => (dispatch) => {
     .catch((e) => dispatch(fetchFiveDayForecastDataReject(e)))
 };
 
-export const fetchCurrentForecast = (payload) => (dispatch) => {
-  fetchCurrentForecastPending();
-  fetchCurrentForecastData(payload)
+export const fetchCurrentWeather = (payload) => (dispatch) => {
+  fetchCurrentWeatherPending();
+  fetchCurrentWeatherData(payload)
   .then(response => response.json())
-  .then(data => dispatch(fetchCurrentForecastDataSuccess(data)))
-  .catch((e) => dispatch(fetchCurrentForecastDataReject(e)))
+  .then(data => dispatch(fetchCurrentWeatherDataSuccess(data)))
+  .catch((e) => dispatch(fetchCurrentWeatherDataReject(e)))
 };
