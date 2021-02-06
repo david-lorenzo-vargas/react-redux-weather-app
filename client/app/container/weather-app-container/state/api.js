54f22aabc1555da77ce8d0c66b886557a86696b4
unknown <dlorenzo.05@gmail.com>
2021-02-06 20:19:13 +0100

    function renamed

--- client/app/container/weather-app-container/state/api.js
+++ client/app/container/weather-app-container/state/api.js
@@ -6,7 +6,7 @@ export const fetchFiveDayForecastData = (payload) => {
   return fetch('https://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid=77c759a539a8027e8d610da397bec1a5', options);
 };
 
-export const fetchCurrentForecastData = (payload) => {
+export const fetchCurrentWeatherData = (payload) => {
   const options = {
     method: 'GET',
   };
