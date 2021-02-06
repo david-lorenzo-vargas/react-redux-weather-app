42437c571d15a0485628c1c2212cc29b56b27951
unknown <dlorenzo.05@gmail.com>
2021-02-06 17:16:28 +0100

    new prop passed down

--- client/app/components/forecast-item/forecast-item.jsx
+++ client/app/components/forecast-item/forecast-item.jsx
@@ -24,6 +24,7 @@ const ForecastItem = (props) => {
                     size="large"
                     uppercase
                     margin
+                    type="day"
                   />
                 </Column>
                 <Column>
