ce6561c16a56fcdcd1bdd621535673754f98a44c
unknown <dlorenzo.05@gmail.com>
2021-02-06 13:56:36 +0100

    props renames and function created to convert countdown received by props in wdith percentage to pass it as inline style

--- client/app/components/progress-bar/progess-bar__item/progress-bar__item.jsx
+++ client/app/components/progress-bar/progess-bar__item/progress-bar__item.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import styles from './progress-bar__item.scss';
 
 const ProgressBarItem = (props) => {
-  const { width } = props;
+  const { seconds } = props;
 
-  const barWidth = {
-    width: `${width}%`,
+  const getWidth = () => {
+    const step = 1;
+    const time = 60;
+    const percentage = (100 * step) / time;
+    const count = time - seconds;
+    const width = count * percentage;
+
+    return width;
   };
 
+  // const barWidth = {
+  //   width: `${getWidth()}%`,
+  // };
+
+  // style={barWidth}
+
   return (
-    <div className={styles['progress-bar']} style={barWidth} />
+    <div className={styles['progress-bar']} />
   );
 };
 
