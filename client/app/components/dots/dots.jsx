682d97ab9d9683b456a1ecb00cfbf2c40de84efd
unknown <dlorenzo.05@gmail.com>
2021-02-06 23:29:40 +0100

    code refactoring

--- client/app/components/dots/dots.jsx
+++ client/app/components/dots/dots.jsx
@@ -7,28 +7,24 @@ import styles from './dots.scss';
 
 const cx = classnames.bind(styles);
 
-const Dots = (props) => {
-  const { dots } = props;
-
-  return (
-    <div className={styles['dots']}>
-      <Row between>
-        <div className={cx('dot', {
-          [`dot--number-${dots[0]}`]: dots,
-        })}
-        />
-        <div className={cx('dot', {
-          [`dot--number-${dots[1]}`]: dots,
-        })}
-        />
-        <div className={cx('dot', {
-          [`dot--number-${dots[2]}`]: dots,
-        })}
-        />
-      </Row>
-    </div>
-  );
-};
+const Dots = ({dots}) => (
+  <div className={styles['dots']}>
+    <Row between>
+      <div className={cx('dot', {
+        [`dot--number-${dots[0]}`]: dots,
+      })}
+      />
+      <div className={cx('dot', {
+        [`dot--number-${dots[1]}`]: dots,
+      })}
+      />
+      <div className={cx('dot', {
+        [`dot--number-${dots[2]}`]: dots,
+      })}
+      />
+    </Row>
+  </div>
+);
 
 Dots.propTypes = {
   number: PropTypes.arrayOf(PropTypes.string),
