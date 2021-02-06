a63502e488b0e992f81b6ad9ef3abd63ecf3370a
unknown <dlorenzo.05@gmail.com>
2021-02-06 17:20:21 +0100

    proptypes removed, unused props removed, indentation fixed, unused props removed from ProgressBarItem component

--- client/app/components/progress-bar/progress-bar.jsx
+++ client/app/components/progress-bar/progress-bar.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { ProgressBarItem } from '@components'
 import styles from './progress-bar.scss';
 
-const ProgressBar = (props) => {
-  const { countdown } = props;
-
-  return (
-    <div className={styles['progress-bar']}>
-      <ProgressBarItem seconds={countdown} />
-    </div>
-  );
-};
-
-ProgressBar.propTypes = {
-  countdown: PropTypes.number,
-};
+const ProgressBar = () => (
+  <div className={styles['progress-bar']}>
+    <ProgressBarItem />
+  </div>
+);
 
 export default ProgressBar;
