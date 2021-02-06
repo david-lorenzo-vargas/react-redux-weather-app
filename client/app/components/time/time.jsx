52102a417465a9a8a3d26c75b22c2c22efcde5d1
unknown <dlorenzo.05@gmail.com>
2021-02-06 23:28:32 +0100

    code refactoring

--- client/app/components/time/time.jsx
+++ client/app/components/time/time.jsx
@@ -5,27 +5,27 @@ import styles from './time.scss';
 import { Row, Column, Text, Dots } from '@components';
 
 const Time = () => (
-    <div className={styles['time-box']}>
-      <Row between>
-        <Column>
-          <Dots dots={['three', 'two', 'one']} />
-        </Column>
-        <Column>
-          <Clock
-            format={'HH:mm'}
-            ticking={true}
-            timezone={'Etc/GMT0'}
-            className={styles['time-box__clock']}
-          />
-        </Column>
-        <Column>
-          <Text text="gmt" uppercase color="white" size="medium" />
-        </Column>
-        <Column>
-          <Dots dots={['one', 'two', 'three']} />
-        </Column>
-      </Row>
-    </div>
-  );
+  <div className={styles['time-box']}>
+    <Row between>
+      <Column>
+        <Dots dots={['three', 'two', 'one']} />
+      </Column>
+      <Column>
+        <Clock
+          format={'HH:mm'}
+          ticking={true}
+          timezone={'Etc/GMT0'}
+          className={styles['time-box__clock']}
+        />
+      </Column>
+      <Column>
+        <Text text="gmt" uppercase color="white" size="medium" />
+      </Column>
+      <Column>
+        <Dots dots={['one', 'two', 'three']} />
+      </Column>
+    </Row>
+  </div>
+);
 
 export default Time;
